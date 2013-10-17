c0295b839de7c2c2a32c0e32fea7a58fd893383f
Galder Zamarreño <galder@zamarreno.com>
2013-10-17 21:35:25 +0200

    Working!! :)

--- buildSrc/src/main/groovy/org/gradle/frege/FregeTask.groovy
+++ buildSrc/src/main/groovy/org/gradle/frege/FregeTask.groovy
@@ -10,6 +10,8 @@ import org.gradle.api.internal.file.FileResolver
 
 class FregeTask extends DefaultTask {
 
+  private static final FREGE_FILE_EXTENSION_PATTERN = ~/.*\.fr?$/
+
   @Input boolean hints
 
   @Input boolean verbose
@@ -22,15 +24,54 @@ class FregeTask extends DefaultTask {
 
   @Input boolean includeStale
 
+  // TODO: Find default
+  @OutputDirectory File outputDir = new File("build/classes")
+
   @TaskAction
   void executeCompile() {
-    println "Compiling Frege"
+    println "Compiling Frege to " + outputDir
 
     FileResolver fileResolver = getServices().get(FileResolver.class)
     JavaExecAction action = new DefaultJavaExecAction(fileResolver)
     action.setMain("frege.compiler.Main")
     action.setClasspath(project.files(project.configurations.compile))
+
+    List args = []
+    if (hints)
+      args << "-hints"
+    if (inline)
+      args << "-inline"
+    if (make)
+      args << "-make"
+    if (verbose)
+      args << "-v"
+    if (skipCompile)
+      args << "-j"
+
+    args << "-d"
+    args << outputDir
+
+
+    eachFileRecurse(new File("src/main/frege")) { File file ->
+      if (file.name =~ FREGE_FILE_EXTENSION_PATTERN) {
+        args << file
+      }
+
+    }
+
+    action.args(args)
+
     action.execute()
+  }
+
+  private static void eachFileRecurse(File dir, Closure fileProcessor) {
+    dir.eachFile { File file ->
+      if (file.directory) {
+        eachFileRecurse(file, fileProcessor)
+      } else {
+        fileProcessor(file)
+      }
+    }
   }
 
 }
