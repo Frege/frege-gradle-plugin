ce9be28ae8c3285c6da33ad1dc620e7d381a52a5
Mark Perry <maperry78@yahoo.com.au>
2015-03-13 00:35:02 +1000

    Use official frege snapshots

--- src/main/groovy/frege/gradle/NativeGenTask.groovy
+++ src/main/groovy/frege/gradle/NativeGenTask.groovy
@@ -3,7 +3,7 @@ package frege.gradle
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 
-//import frege.nativegen.*
+import frege.nativegen.*
 
 /**
  * Created by MarkPerry on 18/02/2015.
@@ -14,11 +14,10 @@ class NativeGenTask extends DefaultTask {
 
     String clazz = null
 
-
     @TaskAction
     void gen() {
 
-//        frege.nativegen.Main.main([clazz] as String[])
+        frege.nativegen.Main.main([clazz] as String[])
     }
 
 
