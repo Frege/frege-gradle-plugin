fd2fee1260a494ea24887eb1512c27938e8fa230
Thibault Gagnaux <tgagnaux@gmail.com>
2021-11-24 08:00:01 +0100

    refactor: allows to configure the classpath with the groovy dependency notation
    e.g:
    
    ```groovy
    dependencies {
        implementation 'org.frege-lang:fregefx:0.8.2-SNAPSHOT'
    }
    ```

--- src/main/java/ch/fhnw/thga/gradleplugins/FregePlugin.java
+++ src/main/java/ch/fhnw/thga/gradleplugins/FregePlugin.java
@@ -2,6 +2,7 @@ package ch.fhnw.thga.gradleplugins;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.tasks.TaskProvider;
 
 public class FregePlugin implements Plugin<Project> {
@@ -10,9 +11,11 @@ public class FregePlugin implements Plugin<Project> {
     public static final String RUN_FREGE_TASK_NAME = "runFrege";
     public static final String FREGE_PLUGIN_ID = "ch.fhnw.thga.frege";
     public static final String FREGE_EXTENSION_NAME = "frege";
+    public static final String FREGE_IMPLEMENTATION_SCOPE = "implementation";
 
     @Override
     public void apply(Project project) {
+        Configuration implementation = project.getConfigurations().create(FREGE_IMPLEMENTATION_SCOPE);
         FregeExtension extension = project.getExtensions().create(FREGE_EXTENSION_NAME, FregeExtension.class);
         TaskProvider<SetupFregeTask> setupFregeCompilerTask = project.getTasks().register(SETUP_FREGE_TASK_NAME,
                 SetupFregeTask.class, task -> {
@@ -28,6 +31,7 @@ public class FregePlugin implements Plugin<Project> {
                     task.getFregeMainSourceDir().set(extension.getMainSourceDir());
                     task.getFregeOutputDir().set(extension.getOutputDir());
                     task.getFregeCompilerFlags().set(extension.getCompilerFlags());
+                    task.getFregeDependencies().set(implementation.getAsPath());
                 });
         project.getTasks().register(RUN_FREGE_TASK_NAME, RunFregeTask.class, task -> {
             task.dependsOn(compileFregeTask);
